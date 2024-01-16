d4f1a327e1cc135f3be4440c5b18822f9606d52e
vt <writetovt7@gmail.com>
2024-01-16 11:40:09 +0530

    Question2

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; // Initialize the number
+        
+        int factorial = 1;
+        
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        
+        System.out.println("The factorial of " + number + " is: " + factorial);
+    }
+}
